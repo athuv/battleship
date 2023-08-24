48f9de923a0c050d4b63d959f9db7faad29cefda
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-08-24 17:31:11 +0530

    refactor: Add type property for ships

--- src/js/shipFactory.js
+++ src/js/shipFactory.js
@@ -1,7 +1,8 @@
 function ship() {
 
-  function createShip(size) {
+  function createShip(size, type) {
     return {
+      type: type,
       size: size,
       life: size,
       isSunked: false,
@@ -13,27 +14,27 @@ function ship() {
   }
 
   function carrier() {
-    const carrier = createShip(5);
+    const carrier = createShip(5, 'CA');
     return carrier;
   }
 
   function battleship() {
-    const battleShip = createShip(4);
+    const battleShip = createShip(4, 'B');
     return battleShip;
   }
 
   function cruiser() {
-    const cruiser = createShip(3);
+    const cruiser = createShip(3, 'CR');
     return cruiser;
   }
 
   function submarine() {
-    const submarine = createShip(3);
+    const submarine = createShip(3, 'S');
     return submarine;
   }
 
   function patrolBoat() {
-    const patrolBoat = createShip(2);
+    const patrolBoat = createShip(2, 'P');
     return patrolBoat;
   }
 
