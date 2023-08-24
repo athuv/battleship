0f192d65648bb152d46f0abd18f0618b206fa21a
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-08-24 22:43:33 +0530

    refactor: change ship placement for carrier and battleship in X axis
    - Implement placement for carrier ship in X axis
    - Implement placement for battleship in X axis

--- src/js/gameBoardFactory.js
+++ src/js/gameBoardFactory.js
@@ -6,13 +6,13 @@ function gameBoard() {
   const EMPTY = 'E';
   const HIT = 'H';
   const MISSED = 'M';
-  // const SHIP_TYPE = {
-  //   CARRIER_SHIP: 'CA',
-  //   BATTLE_SHIP: 'B',
-  //   CRUISER_SHIP: 'CR',
-  //   SUBMARINE_SHIP: 'S',
-  //   PATROL_BOAT_SHIP: 'P'
-  // }
+  const SHIPS = {
+    CARRIER_SHIP: 'carrier',
+    BATTLE_SHIP: 'battleship',
+    CRUISER_SHIP: 'cruiser',
+    SUBMARINE_SHIP: 'submarine',
+    PATROL_BOAT_SHIP: 'patrolBoat'
+  }
     
   const BOARD = Array.from({ length: ROWS }, () =>
     Array.from({ length: COLS }, () => EMPTY)
@@ -23,23 +23,32 @@ function gameBoard() {
   }
 
   function getShip(shipType) {
-    if(['carrier', 'battleship', 'cruiser', 'submarine', 'patrolBoat'].includes(shipType)){
+    if([SHIPS.CARRIER_SHIP, SHIPS.BATTLE_SHIP, SHIPS.CRUISER_SHIP, SHIPS.SUBMARINE_SHIP, SHIPS.PATROL_BOAT_SHIP].includes(shipType)){
       return ship()[shipType]();
     }
     return false;
   }
 
   function placeShip(shipType, position, axis) {
-    if((shipType === CARRIER_SHIP) && (axis === 'x') && (position[1] >= 2) && (position[1] <= 7) ){
-      BOARD[position[0]][position[1] - 2] = CARRIER_SHIP;
-      BOARD[position[0]][position[1] - 1] = CARRIER_SHIP;
-      BOARD[position[0]][position[1]] = CARRIER_SHIP;
-      BOARD[position[0]][position[1] + 1] = CARRIER_SHIP;
-      BOARD[position[0]][position[1] + 2] = CARRIER_SHIP;
-      return getBoard();
-    }else{
-      return 'cannot be placed here'
+    if(canPlaceShip(shipType, position, axis)) {
+      if(axis === 'x') {
+        if(shipType === SHIPS.CARRIER_SHIP) {
+          for (let offset = -2; offset <= 2; offset++) {
+            BOARD[position[0]][position[1] + offset] = getShip(shipType).type;
+          }          
+        }else if(shipType === SHIPS.BATTLE_SHIP) {
+          for (let offset = -1; offset <= 2; offset++) {
+            BOARD[position[0]][position[1] + offset] = getShip(shipType).type;
+          } 
+        }else if([SHIPS.CRUISER_SHIP, SHIPS.CRUISER_SHIP].includes(shipType)) {
+
+        }
+
+        return BOARD;
+      }
     }
+
+    return 'Ship Cannot Be Placed Here';
   }
 
   function canPlaceShip(shipType, position, axis) {
@@ -47,15 +56,29 @@ function gameBoard() {
     if(getShip(shipType) === false) return 'Ship Not Found!';
 
     if(axis === 'x') {
-      if((shipType === 'carrier') && (position[1] >= 2) && (position[1] <= 7)) {
+      if((shipType === SHIPS.CARRIER_SHIP) && (position[1] >= 2) && (position[1] <= 7)) {
+        // Checking for collision
+        for (let offset = -2; offset <= 2; offset++) {
+          if(BOARD[position[0]][position[1] + offset] !== EMPTY) {
+            return false;
+          }
+        }
         return true;
-      }else if(['battleship', 'cruiser', 'submarine'].includes(shipType)  && (position[1] >= 1) && (position[1] <= 8)) {
+      }else if((shipType === SHIPS.BATTLE_SHIP)  && (position[1] >= 1) && (position[1] <= 7)) {
+        // Checking for collision
+        for (let offset = -1; offset <= 2; offset++) {
+          if(BOARD[position[0]][position[1] + offset] !== EMPTY) {
+            return false;
+          }
+        }
         return true;
-      }else if((shipType === 'patrolBoat') && (position[1] >= 0) && (position[1] <= 9)) {
+      }else if(([SHIPS.CRUISER_SHIP, SHIPS.SUBMARINE_SHIP].includes(shipType))  && (position[1] >= 1) && (position[1] <= 8)) {
+
+      } else if((shipType === SHIPS.PATROL_BOAT_SHIP) && (position[1] >= 0) && (position[1] <= 9)) {
         return true;
       }
     }else {
-      if((shipType === 'carrier') && (position[0] >= 2) && (position[0] <= 7)) {
+      if((shipType === SHIPS.CARRIER_SHIP) && (position[0] >= 2) && (position[0] <= 7)) {        
         return true;
       }else if(['battleship', 'cruiser', 'submarine'].includes(shipType)  && (position[0] >= 1) && (position[0] <= 8)) {
         return true;
