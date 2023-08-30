818ef4eb7865de6ac2bc8f72aff894a76dfdba28
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-08-30 17:51:23 +0530

    test: test createPlayer feature

---
+++ __tests__/playerFactory.test.js
@@ -0,0 +1,17 @@
+import player from "../src/js/playerFactory";
+
+const playerOneName = 'John';
+const playerTwoName = 'Kevin';
+
+describe('creating players', () => {
+  const playerInstance = player();
+  const playerOne = playerInstance.createPlayer(playerOneName);
+  const playerTwo = playerInstance.createPlayer(playerTwoName);
+  test('Create player One', () => {
+    expect(playerOne.name).toBe(playerOneName);
+  });
+
+  test('Create player Two', () => {
+    expect(playerTwo.name).toBe(playerTwoName);
+  });
+});
