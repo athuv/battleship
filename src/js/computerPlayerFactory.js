e54b404dde1fb8242520e90722b25c9d777e128a
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-09-01 23:59:58 +0530

    feat: Implement random attack functionality for computer player

--- src/js/computerPlayerFactory.js
+++ src/js/computerPlayerFactory.js
@@ -1,8 +1,10 @@
 import gameBoard from "./gameBoardFactory.js";
+import { CELL_STATES } from "./config.js";
 
 function computerPlayer() {
 
   let gameBoardInstance;
+  const possibleAttacks = [];
   
   function setGameBoardInstance() {
     if(!gameBoardInstance) gameBoardInstance = gameBoard();
@@ -13,8 +15,37 @@ function computerPlayer() {
     return gameBoardInstance.getBoard();
   }
 
+  function getPossibleAttacks() {
+    if(possibleAttacks.length === 0){
+      getBoard().forEach((row, rowIndex) => {
+        row.forEach((cell, colIndex) => {
+          if(cell === CELL_STATES.EMPTY) possibleAttacks.push([rowIndex, colIndex])
+        });
+      });
+    }
+    return possibleAttacks;
+  }
+  
+  function removePossibleAttack(index) {
+    possibleAttacks.splice(index, 1);
+  }
+
+  function randomAttack() {
+    const randomIndex = Math.floor(Math.random() * getPossibleAttacks().length);
+    const randomPossibleAttack = getPossibleAttacks()[randomIndex];
+    gameBoardInstance.receiveAttack(randomPossibleAttack[0], randomPossibleAttack[1]);
+    removePossibleAttack(randomIndex);
+    return randomPossibleAttack;
+  }
+
+
+
+
+  // remove  possibleAttacks, generatePossibleAttacks
   return {
-    getBoard
+    getBoard,
+    getPossibleAttacks,
+    randomAttack
   }
 }
 
