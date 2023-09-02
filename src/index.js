8f92af6356457c2028b37286f7b1954905b47af4
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-09-02 17:07:11 +0530

    test: Add temporary code for testing purposes in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import './css/styles.css';
+import gameBoard from './js/gameBoardFactory.js';
+import player from './js/playerFactory.js';
+import computerPlayer from './js/computerPlayerFactory.js';
             
 function component() {
+  const gameBoardInstance = gameBoard();
+  const playerInstance = player();
+  const computerPlayerInstance = computerPlayer();
+
+  const playerOne = playerInstance.createPlayer('John');
+  const playerTwo = playerInstance.createPlayer('Kevin');
+
+  const gameBoardPlayerOne = gameBoardInstance;
+  const gameBoardPlayerTwo = computerPlayerInstance;
+  
+  computerPlayerInstance.setPlayerOneGameBoardInstance(gameBoardPlayerOne);
+
+  window.p1gb = gameBoardPlayerOne;
+  window.p2gb = gameBoardPlayerTwo;
+
   const element = document.createElement('div');
-  element.classList.add('hello');
   return element;
 }
             
