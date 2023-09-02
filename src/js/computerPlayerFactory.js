fadbbf3f2d7e84d341532dc7cd7188f861ff2b1b
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-09-02 14:51:07 +0530

    feat: Implement automatic ship placement for computer player

--- src/js/computerPlayerFactory.js
+++ src/js/computerPlayerFactory.js
@@ -1,5 +1,5 @@
 import gameBoard from "./gameBoardFactory.js";
-import { CELL_STATES, MESSAGES, SHIPS, AXIS } from "./config.js";
+import { CELL_STATES, MESSAGES, SHIPS, AXIS, SHIP_RANGE } from "./config.js";
 
 function computerPlayer() {
 
@@ -56,27 +56,54 @@ function computerPlayer() {
     return false;
   }
 
+  function getRandomCoordInRange(min, max) {
+    const row =  Math.floor(Math.random() * (max - min + 1)) + min;
+    const col =  Math.floor(Math.random() * (max - min + 1)) + min;
+
+    return [row, col];
+  }
+
   function placeComputerShips() {
+    getGameBoardInstance().resetBoard();
     if(getGameBoardInstance().areShipsPlaced() === true) return MESSAGES.ALREADY_PLACED;
-    
-    getGameBoardInstance().placeShip(SHIPS.CARRIER_SHIP, [2, 4], AXIS.Y);
-    getGameBoardInstance().placeShip(SHIPS.BATTLE_SHIP, [5, 2], AXIS.X);
-    getGameBoardInstance().placeShip(SHIPS.CRUISER_SHIP, [8, 2], AXIS.Y);
-    getGameBoardInstance().placeShip(SHIPS.SUBMARINE_SHIP, [8, 1], AXIS.Y);
-    getGameBoardInstance().placeShip(SHIPS.PATROL_BOAT_SHIP, [8, 0], AXIS.Y);
+    const ships = [SHIPS.CARRIER_SHIP, SHIPS.BATTLE_SHIP, SHIPS.CRUISER_SHIP, SHIPS.SUBMARINE_SHIP, SHIPS.PATROL_BOAT_SHIP];
+
+    ships.forEach((ship) => {
+      let isPlaced = false;
+      let range = [];
+
+      while(!isPlaced) {
+        const randomAxis = Math.random() < 0.5 ? AXIS.X : AXIS.Y;
+
+        const gameBoardInstance = getGameBoardInstance();
+        if(ship === SHIPS.CARRIER_SHIP) range = getRandomCoordInRange(SHIP_RANGE.CARRIER.MIN_RANGE, SHIP_RANGE.CARRIER.MAX_RANGE);
+        if(ship === SHIPS.BATTLE_SHIP) range = getRandomCoordInRange(SHIP_RANGE.BATTLESHIP.MIN_RANGE, SHIP_RANGE.BATTLESHIP.MAX_RANGE);
+        if(ship === SHIPS.CRUISER_SHIP) range = getRandomCoordInRange(SHIP_RANGE.CRUISER_SUBMARINE.MIN_RANGE, SHIP_RANGE.CRUISER_SUBMARINE.MAX_RANGE);
+        if(ship === SHIPS.SUBMARINE_SHIP) range = getRandomCoordInRange(SHIP_RANGE.CRUISER_SUBMARINE.MIN_RANGE, SHIP_RANGE.CRUISER_SUBMARINE.MAX_RANGE);
+        if(ship === SHIPS.PATROL_BOAT_SHIP) range = getRandomCoordInRange(SHIP_RANGE.PATROL_BOAT.MIN_RANGE, SHIP_RANGE.PATROL_BOAT.MAX_RANGE);
+        
+        const shipPlaced = gameBoardInstance.placeShip(ship, range, randomAxis);
+
+        if(shipPlaced === true) isPlaced = true;
+       
+      }
+    });
+
   }
 
 
 
 
-  // remove  possibleAttacks, generatePossibleAttacks, getPlayerOneGameBoardInstance
+  // remove  possibleAttacks, generatePossibleAttacks, getPlayerOneGameBoardInstance, getRandomCoordInRange, getGameBoardInstance
   return {
     getBoard,
     getPossibleAttacks,
     randomAttack,
     setPlayerOneGameBoardInstance,
     getPlayerOneGameBoardInstance,
-    placeComputerShips
+    placeComputerShips,
+    getRandomCoordInRange,
+    getGameBoardInstance
   }
 }
 
