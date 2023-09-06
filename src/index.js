a225f8ac5b4bf87bea68d24eb0e2aec6146be412
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-09-06 21:31:52 +0530

    feat: add bottom section & left grid container click event listener

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import './css/styles.css';
 import * as domManager from './js/utils/domUtils.js';
 import createTopSection from './js/components/section-top.js';
-import createMiddleSection from './js/components/section-middle.js';
+import {createMiddleSection, middleSectionEventListeners} from './js/components/section-middle.js';
+import createBottomSection from './js/components/section-bottom.js';
 
 function component() {
   const bodyElement = document.body;
   domManager.appendChildElements(
     bodyElement,
     createTopSection(),
-    createMiddleSection()
+    createMiddleSection(),
+    createBottomSection()
   );
-  
+
+  middleSectionEventListeners();
   return bodyElement;
 }
             
