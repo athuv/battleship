e479cfb06ebe65e6de084385b23820daeeaa9fc0
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-09-06 16:40:11 +0530

    feat: add top section that contains header and player names

---
+++ src/js/components/section-top.js
@@ -0,0 +1,45 @@
+import * as domManager from '../utils/domUtils.js';
+
+function createPlayerOne() {
+  const playerOneP = domManager.createParagraphElement(['player-one']);
+  const playerOneSpan = domManager.createSpanElement(
+    ['player-one__name'],
+    {innerText: 'You'}    
+  );
+
+  domManager.appendChildElements(playerOneP, playerOneSpan);
+  return playerOneP;
+}
+
+function createHeadingP() {
+  const headingP = domManager.createParagraphElement(
+    ['top-heading'],
+    {innerText: 'Battleship'}
+  );
+
+  return headingP;
+}
+
+function createPlayerTwo() {
+  const playerTwoP = domManager.createParagraphElement(['player-two']);
+  const playerTwoSpan = domManager.createSpanElement(
+    ['player-two__name'],
+    {innerText: 'Computer'}    
+  );
+
+  domManager.appendChildElements(playerTwoP, playerTwoSpan);
+  return playerTwoP;
+}
+
+function createTopSection() {
+  const sectionTop = domManager.createSectionElement(
+    ['section-top']
+  );
+
+  domManager.appendChildElements(sectionTop, createPlayerOne(), createHeadingP(), createPlayerTwo());
+  return sectionTop;
+}
+
+export {
+  createTopSection
+}
