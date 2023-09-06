0650b6e74933b0e5f802cedfbd4cf568b3c35b43
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-09-06 20:51:02 +0530

    feat: add middle section by implementing createMiddleSection() function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import './css/styles.css';
 import * as domManager from './js/utils/domUtils.js';
-import { createTopSection } from './js/components/section-top.js';
+import createTopSection from './js/components/section-top.js';
+import createMiddleSection from './js/components/section-middle.js';
 
 function component() {
   const bodyElement = document.body;
-  domManager.appendChildElements(bodyElement, createTopSection())
-
+  domManager.appendChildElements(
+    bodyElement,
+    createTopSection(),
+    createMiddleSection()
+  );
+  
   return bodyElement;
 }
             
