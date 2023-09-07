7545c029fda3cb1447bb2699280a0955b3ed1a80
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-09-07 21:40:35 +0530

    feat: implement player one info & ship placement popup in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import './css/normalize.css'
 import './css/styles.css';
 import * as domManager from './js/utils/domUtils.js';
 import createTopSection from './js/components/section-top.js';
 import {createMiddleSection, middleSectionEventListeners} from './js/components/section-middle.js';
 import createBottomSection from './js/components/section-bottom.js';
+import { createPopup, popupEventListeners } from './js/components/popup-player-ships-placement.js';
 
 function component() {
   const bodyElement = document.body;
@@ -10,10 +12,12 @@ function component() {
     bodyElement,
     createTopSection(),
     createMiddleSection(),
-    createBottomSection()
+    createBottomSection(),
+    createPopup()
   );
 
   middleSectionEventListeners();
+  popupEventListeners();
   return bodyElement;
 }
             
