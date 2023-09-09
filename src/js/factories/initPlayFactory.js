d647f5d9dff3c71c1d0569fc73da17bdec1d334d
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-09-09 18:51:07 +0530

    feat: add play() function in initPlayFactory.js

--- src/js/factories/initPlayFactory.js
+++ src/js/factories/initPlayFactory.js
@@ -5,7 +5,18 @@ const playerComputerGameBoard = getComputerPlayerFactoryInstance();
 const playerTwoInstance = playerComputerGameBoard.getGameBoardInstance();
 const playerNameInstance = getPlayerFactoryInstance();
 
+const currentPlayer = 1;
 window.q = playerTwoInstance;
+
+function switchPlayer() {
+  currentPlayer = (currentPlayer === 1) ? 2 : 1;
+}
+
+function play() {
+  if(currentPlayer === 1){
+    console.log(playerNameInstance.getPlayerOne().name);
+  }
+}
 
 function initPlay() {
   playerComputerGameBoard.placeComputerShips();
@@ -13,8 +24,10 @@ function initPlay() {
 
  if((playerNameInstance.getPlayerOne().name) && (playerTwoInstance.areShipsPlaced()) && playerOneGameBoard.areShipsPlaced()){
   isCanStartGame = true;
+  play();
   return true;
  }
+
 }
 
 export default initPlay;
