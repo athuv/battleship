ebade97cf1310fa0fd05f73361fbaf9d3345bd73
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-09-09 17:44:32 +0530

    feat: add feature to set and get players name and position
    - refactor createPlayer() function to create players
    - add getPlayerOne() and getPlayerTwo() to get players

--- src/js/factories/playerFactory.js
+++ src/js/factories/playerFactory.js
@@ -1,18 +1,31 @@
 function player() {
-  let playerName;
+  let playerOneName;
+  let playerTwoName;
 
-  function createPlayer(name) {
-    playerName = name;
+  function createPlayer(name, playerPosition) {
+    if(playerPosition === 1) playerOneName = name;
+    if(playerPosition === 2) playerTwoName = name;
   }
 
 
-  function getPlayerName() {
-    return playerName;
+  function getPlayerOne() {
+    return {
+      name: playerOneName,
+      position: 1
+    };
+  }
+
+  function getPlayerTwo() {
+    return {
+      name: playerTwoName,
+      position: 2
+    };
   }
 
   return {
     createPlayer,
-    getPlayerName
+    getPlayerOne,
+    getPlayerTwo
   }
 }
 
