0e6f0b9533d7bd0b3d8ac8b3c8a94961b1ec3667
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-09-09 17:47:04 +0530

    test: test player set and get feature implemented in [ebade97c]

--- __tests__/playerFactory.test.js
+++ __tests__/playerFactory.test.js
@@ -7,7 +7,14 @@ describe('creating players', () => {
 
 
   test('Create player One', () => {
-    playerOne.createPlayer('Ahamed');
-    expect(playerOne.getPlayerName()).toBe('Ahamed');
+    playerOne.createPlayer('Ahamed', 1);
+    expect(playerOne.getPlayerOne().name).toBe('Ahamed');
+    expect(playerOne.getPlayerOne().position).toBe(1);
+  });
+
+  test('Create player Two', () => {
+    playerOne.createPlayer('Ahamed2', 2);
+    expect(playerTwo.getPlayerTwo().name).toBe('Ahamed2');
+    expect(playerTwo.getPlayerTwo().position).toBe(2);
   });
 });
