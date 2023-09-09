8815f44adc0e78ada3d07836d533e7fe75a128e2
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-09-09 21:08:04 +0530

    feat: attach middle section event listeners

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './css/normalize.css'
 import './css/styles.css';
 import * as domManager from './js/utils/domUtils.js';
 import createTopSection from './js/components/section-top.js';
-import {createMiddleSection} from './js/components/section-middle.js';
+import {createMiddleSection, middleSectionEvents} from './js/components/section-middle.js';
 import createBottomSection from './js/components/section-bottom.js';
 import { createPopup, popupEventListeners } from './js/components/popup-player-ships-placement.js';
 
@@ -17,6 +17,7 @@ function component() {
   );
 
   popupEventListeners();
+  middleSectionEvents();
   return bodyElement;
 }
             
