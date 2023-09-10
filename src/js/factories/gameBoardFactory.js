91fccc1bbd6539a363b6e257aa90eb2a379dae11
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-09-10 18:33:46 +0530

    fix: fix bugs by implementing shipInstnace from instanceRegistry.js

--- src/js/factories/gameBoardFactory.js
+++ src/js/factories/gameBoardFactory.js
@@ -1,11 +1,10 @@
-import shipFactory from './shipFactory.js';
 import { GRID, CELL_STATES, SHIP, ERROR_MESSAGES, AXIS, MESSAGES } from '../utils/config.js';
-
+import { getShipFactoryInstance } from '../utils/instanceRegistry.js';
 function gameBoard() {
-
+debugger;
   // Make sure ships only placed once
   let placedShips = new Set(); 
-  const shipInstance = shipFactory();
+  const shipInstance = getShipFactoryInstance();
 
   function createShipInstances() {
     const shipInstances = {
